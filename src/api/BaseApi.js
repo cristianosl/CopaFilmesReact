aa445b79c7846febfa8db6fc6f6f5352cf54414a
Cristiano Lima <cristianosl@gmail.com>
2019-06-15 17:59:10 -0300

    Atualizado o host

--- src/api/BaseApi.js
+++ src/api/BaseApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default class BaseApi {
   constructor() {
     this.jsonRequest = axios.create({
-      baseURL: "https://localhost:44380/api",
+      baseURL: "http://localhost:62956/api",
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json"
